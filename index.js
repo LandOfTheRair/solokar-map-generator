8f549b9bb15e6d03ffb48e779f2cd805d86fc466
Kyle Kemp <kyle@seiyria.com>
2022-02-28 12:15:19 -0600

    Add config file

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 
 const ROT = require('rot-js');
 const fs = require('fs-extra');
+const yaml = require('js-yaml');
 
 const spriteData = require('./sprite-data.json');
+const config = yaml.load(fs.readFileSync('./config.yml'));
 
 const genWidth = 100;
 const genHeight = 100;
 const gutter = 5;
+
+Object.keys(config.decor).forEach(decorKey => config.decor[decorKey] = config.decor[decorKey].flat());
+
+Object.keys(config.floors).forEach(floorKey => {
+  if(config.floors[floorKey].allowFluids) config.floors[floorKey].fluids = config.floors[floorKey].fluids.flat();
+  if(config.floors[floorKey].allowTrees) config.floors[floorKey].trees = config.floors[floorKey].trees.flat();
+
+  config.floors[floorKey].decor = config.floors[floorKey].decor.flat();
+});
+
+config.configs.roomDecor.forEach(({ decors }) => {
+  decors.forEach(decorConfig => {
+    decorConfig.decor = decorConfig.decor.flat();
+  });
+});
 
 /*
 TODO:
@@ -18,254 +35,20 @@ TODO:
 - add succorport to prevent everything
 - add spawners (rarely, add monsters that fight with these monsters - ie heniz/steffen, crazed/not)
 - add loot
-- add random green npcs (trainers, etc; healer trainer must not recall)
+- add random green npcs (trainers, etc; healer trainer must not recall, detect-giving npc for mazes, etc)
 */
 
-const foliage = {
-  apple:          [1998, 1999, 2000, 2001, 2002, 2003],
-  fall:           [2004, 2005, 2006, 2007, 2012, 2013, 2014, 2015, 2016, 2017, 2018],
-  dead:           [2133, 2134, 2135, 2136, 2137, 2138, 2139, 2140, 2172, 2173, 2174, 2175, 2177, 2178, 2179, 2180, 2146, 2147, 2148, 2149, 2150, 2151, 2152, 2153, 2159, 2160, 2161, 2162, 2163, 2164, 2166, 2167],
-  evergreen:      [2169, 2170, 2171, 2141, 2142, 2144, 2145, 2181, 2182, 2183, 2184, 2155, 2156, 2157, 2158, 2168]
-};
-
-const fluids = {
-  water:          { spriteStart: 384 },
-  darkwater:      { spriteStart: 768 },
-  lava:           { spriteStart: 432 },
-};
-
-const decor = {
-  furrug:         [1701, 1702, 1703, 1704, 1705, 1706, 1707, 1708, 1709, 1710, 1711, 1712],
-  bed:            [1729, 1730, 1731, 1732, 1733, 1734, 1735, 1736, 1737, 1738, 1739, 1740, 1725, 1726, 1727, 1728, 1517, 1518, 1519, 1520],
-  pillar:         [2259, 2260, 2261, 2210],
-  grave:          [1761, 1762, 1763, 1764, 1765, 1766, 1767, 1768, 1769, 1770, 1771, 1772, 1773, 1774, 1775, 1776, 1796, 1797, 1798, 1799, 1800, 1801],
-  fountain:       [2226, 2227, 2228, 2229],
-  barrel:         [2374, 2375, 2206],
-  misc:           [1613, 1614, 1615, 1616, 2211, 2212, 2213, 2214, 2215, 2209, 2223, 2224, 2105, 1840, 1841, 1842, 1843, 1844, 1845, 1846, 1847, 1848, 2207, 2208, 2203, 1838, 1839, 1849, 1850, 1851, 1852, 1853, 1854, 1855],
-  furniture:      [1581, 1582, 1583, 1584, 1585, 1586, 1587, 1588, 1589, 1590, 1591, 1592, 1593, 1594, 1595, 1596, 1597, 1598, 1599, 1600],
-  water:          [2275, 2276, 2277, 2278, 2279, 2280],
-  oil:            [2281, 2282, 2283, 2284, 2285, 2286],
-  blood:          [2287, 2288, 2289, 2290, 2291],
-  directional:    [1501, 1502, 1503, 1504, 1505, 1506, 1507, 1508, 1509, 1510, 1511, 1512, 1513, 1514, 1515, 1516, 1521, 1522, 1523, 1524, 1529, 1530, 1531, 1532, 1533, 1534, 1535, 1536, 1537, 1538, 1539, 1540, 1541, 1542, 1543, 1544, 1601, 1602, 1603, 1604]
-};
-
-decor.all = Object.values(decor).reduce((a, b) => a.concat(b), []);
-decor.town = [...decor.furrug, ...decor.bed, ...decor.barrel, ...decor.furniture, ...decor.directional];
-
 // every possible room type (for digger maze rooms)
-const roomDecorConfigs = [
-  { name: 'Bedroom', decors: [
-    { quantity: [1, 2], decor: [...decor.bed] },
-    { quantity: [2], decor: [...decor.furniture] },
-    { quantity: [4], decor: [...decor.furrug] },
-  ] },
-  { name: 'Barracks', decors: [
-    { quantity: [7, 8, 9, 10], decor: [...decor.bed] },
-  ] },
-  { name: 'Barracks w/ extra', decors: [
-    { quantity: [4, 5, 6], decor: [...decor.bed] },
-    { quantity: [4, 5, 6], decor: [...decor.furrug, ...decor.furniture] },
-  ] },
-  { name: 'Misc', decors: [
-    { quantity: [4, 5, 6, 7, 8, 9, 10], decor: [...decor.misc] },
-  ] },
-  { name: 'Lounge', decors: [
-    { quantity: [4, 5, 6, 7, 8, 9, 10, 11, 12], decor: [...decor.furniture, ...decor.furrug] },
-  ] },
-  { name: 'Storage', decors: [
-    { quantity: [3, 4, 5, 6, 7, 8, 9, 10], decor: [...decor.barrel] },
-  ] },
-  { name: 'Struggle', decors: [
-    { quantity: [5, 6, 7, 8, 9, 10], decor: [...decor.blood] },
-  ] },
-  { name: 'Spill', decors: [
-    { quantity: [2, 3, 4, 5, 6, 7, 8, 9], decor: [...decor.water] },
-  ] },
-  { name: 'Utilities', decors: [
-    { quantity: [4, 5, 6, 7, 8, 9, 10], decor: [...decor.directional] },
-  ] }
-];
-
-// each possible theme floor
-const floors = {
-  darktile:       { spriteStart: 0,   allowFluids: true, fluids: [fluids.water, fluids.lava, fluids.darkwater], decor: [decor.town] },
-  sand:           { spriteStart: 48,  allowFluids: true, fluids: [fluids.water, fluids.lava], decor: [decor.oil, decor.blood, decor.water, decor.barrel] },
-  nicetile:       { spriteStart: 96,  allowFluids: true, fluids: [fluids.water, fluids.lava, fluids.darkwater], decor: [decor.town] },
-  wood:           { spriteStart: 144, decor: [decor.town] },
-  mist:           { spriteStart: 288, decor: [] },
-  grassair:       { spriteStart: 576, allowFluids: true, fluids: [fluids.water, fluids.darkwater], allowTrees: true, trees: [foliage.apple, foliage.fall, foliage.dead, foliage.evergreen], decor: [decor.grave, decor.directional, decor.misc] },
-  cobblestone:    { spriteStart: 672, allowFluids: true, fluids: [fluids.water, fluids.lava], decor: [decor.town]  },
-  snow:           { spriteStart: 720, allowFluids: true, fluids: [fluids.darkwater], allowTrees: true, trees: [foliage.dead, foliage.evergreen], decor: [decor.grave, decor.misc, decor.directional] },
-  flowergrass:    { spriteStart: 816, allowFluids: true, fluids: [fluids.water, fluids.darkwater], allowTrees: true, trees: [foliage.apple], decor: [decor.misc] },
-  deepgrass:      { spriteStart: 864, allowFluids: true, fluids: [fluids.water, fluids.darkwater], decor: [decor.misc] },
-  swamp:          { spriteStart: 912, allowFluids: true, fluids: [fluids.water, fluids.darkwater], allowTrees: true, trees: [foliage.dead, foliage.evergreen], decor: [decor.grave, decor.misc, decor.directional] },
-};
-
-// each possible theme wall
-const walls = {
-  ether:          { spriteStart: 0,   allowDoors: true, doorStart: 8,   allowHiddenWalls: true },
-  undead:         { spriteStart: 16,  allowDoors: true, doorStart: 10,  allowHiddenWalls: true },
-  cave:           { spriteStart: 48,  allowDoors: true, doorStart: 12,  allowHiddenWalls: true,  allowEmptyWalls: true },
-  stone:          { spriteStart: 64,  allowDoors: true, doorStart: 14,  allowHiddenWalls: true },
-  goldstone:      { spriteStart: 80,  allowDoors: true, doorStart: 16 },
-  town:           { spriteStart: 96,  allowDoors: true, doorStart: 18,  allowHiddenWalls: true },
-  nicetown:       { spriteStart: 160, allowDoors: true, doorStart: 24,  allowHiddenWalls: true },
-  nicetownwhite:  { spriteStart: 176, allowDoors: true, doorStart: 26 },
-  nicestone:      { spriteStart: 192, allowDoors: true, doorStart: 28 },
-  tent:           { spriteStart: 208, allowDoors: true, doorStart: 6  },
-  vibrant:        { spriteStart: 224, allowEmptyWalls: true },
-  icestone:       { spriteStart: 272, allowEmptyWalls: true },
-  tree:           { spriteStart: 288, allowEmptyWalls: true },
-  library:        { spriteStart: 320 },
-  goldcave:       { spriteStart: 336, allowDoors: true, doorStart: 30,  allowHiddenWalls: true },
-};
+const roomDecorConfigs = config.configs.roomDecor;
 
 // each possible theme config
-const themes = {
-  basictown:        { floor: floors.wood,         wall: walls.town,            },
-  basictown2:       { floor: floors.wood,         wall: walls.nicetown,        },
-  basictown3:       { floor: floors.wood,         wall: walls.nicetownwhite,   },
-  
-  cobbletown:       { floor: floors.cobblestone,  wall: walls.town,            },
-  cobbletown2:      { floor: floors.cobblestone,  wall: walls.nicetown,        },
-  cobbletown3:      { floor: floors.cobblestone,  wall: walls.nicetownwhite,   },
-  
-  darkcobbletown:   { floor: floors.darktile,     wall: walls.town,            },
-  darkcobbletown2:  { floor: floors.darktile,     wall: walls.nicetown,        },
-  darkcobbletown3:  { floor: floors.darktile,     wall: walls.nicetownwhite,   },
-
-  dungeon:          { floor: floors.nicetile,     wall: walls.stone,           }, 
-  dungeon2:         { floor: floors.nicetile,     wall: walls.goldstone,       },
-  dungeon3:         { floor: floors.nicetile,     wall: walls.icestone },
-  dungeon4:         { floor: floors.darktile,     wall: walls.stone,           },
-  dungeon5:         { floor: floors.darktile,     wall: walls.goldstone,       },
-  dungeon6:         { floor: floors.darktile,     wall: walls.icestone         },
-  dungeon7:         { floor: floors.cobblestone,  wall: walls.stone,           },
-  dungeon8:         { floor: floors.cobblestone,  wall: walls.goldstone,       },
-  dungeon9:         { floor: floors.cobblestone,  wall: walls.icestone         },
-  dungeon10:        { floor: floors.nicetile,     wall: walls.ether,           },
-  dungeon11:        { floor: floors.nicetile,     wall: walls.goldcave,        },
-  
-  undeadtown:       { floor: floors.darktile,     wall: walls.undead,          },
-  undeadtown2:      { floor: floors.nicetile,     wall: walls.undead,          },
-  
-  desert:           { floor: floors.sand,         wall: walls.undead,          },
-  deserttree:       { floor: floors.sand,         wall: walls.tree,            },
-  deserttown:       { floor: floors.sand,         wall: walls.stone,           },
-  deserttown2:      { floor: floors.sand,         wall: walls.nicestone,       },
-  deserttent:       { floor: floors.sand,         wall: walls.tent,            },
-
-  forest:           { floor: floors.grassair,     wall: walls.tree,            },
-  forest2:          { floor: floors.grassair,     wall: walls.cave,            },
-  forest3:          { floor: floors.grassair,     wall: walls.stone,           },
-  forest4:          { floor: floors.swamp,        wall: walls.tree,            },
-  forest5:          { floor: floors.swamp,        wall: walls.cave,            },
-  forest6:          { floor: floors.swamp,        wall: walls.stone,           },
-  forest7:          { floor: floors.flowergrass,  wall: walls.tree,            },
-  forest8:          { floor: floors.flowergrass,  wall: walls.cave,            },
-  forest9:          { floor: floors.flowergrass,  wall: walls.stone,           },
-  forest10:         { floor: floors.deepgrass,    wall: walls.tree,            },
-  forest11:         { floor: floors.deepgrass,    wall: walls.cave,            },
-  forest12:         { floor: floors.deepgrass,    wall: walls.stone,           },
-
-  mountain:         { floor: floors.snow,         wall: walls.tree,            },
-  mountain2:        { floor: floors.snow,         wall: walls.cave,            },
-  mountain3:        { floor: floors.snow,         wall: walls.stone,           },
-  mountain4:        { floor: floors.snow,         wall: walls.icestone         },
-
-  library:          { floor: floors.wood,         wall: walls.library          },
-
-  vibrant:          { floor: floors.nicetile,     wall: walls.vibrant,         },
-  vibrant2:         { floor: floors.mist,         wall: walls.vibrant,         },
-};
+const themes = config.configs.themes;
 
 // each possible dungeon algo config
-const configs = [
-  {
-    name: 'Digger Maze',
-    algo: 'Digger',
-    algoArgs: [genWidth, genHeight, { roomWidth: [5, 10], roomHeight: [5, 10], corridorLength: [3, 10], dugPercentage: 0.35 }],
-    iterations: 1,
-    doors: true
-  },
-  {
-    name: 'Digger Maze, More Rooms',
-    algo: 'Digger',
-    algoArgs: [genWidth, genHeight, { roomWidth: [3, 7], roomHeight: [3, 7], corridorLength: [6, 15], dugPercentage: 0.5 }],
-    iterations: 1,
-    doors: true
-  },
-  {
-    name: 'Uniform Maze',
-    algo: 'Uniform',
-    algoArgs: [genWidth, genHeight, { roomWidth: [4, 7], roomHeight: [4, 7], roomDugPercentage: 0.7 }],
-    iterations: 1,
-    doors: true
-  },
-  {
-    name: 'Cavelike, Open',
-    algo: 'Cellular',
-    algoArgs: [genWidth, genHeight, { connected: true }],
-    randomize: 0.4,
-    iterations: 1,
-    connect: true
-  },
-  {
-    name: 'Cavelike, Organic Dug-In',
-    algo: 'Cellular',
-    algoArgs: [genWidth, genHeight, { connected: true }],
-    randomize: 0.6,
-    iterations: 1,
-    connect: true
-  },
-  {
-    name: 'Cavelike, Cavernous',
-    algo: 'Cellular',
-    algoArgs: [genWidth, genHeight, { connected: true, born: [4, 5, 6, 7, 8], survive: [2, 3, 4, 5] }],
-    randomize: 0.7,
-    iterations: 1,
-    connect: true
-  },
-  {
-    name: 'Cavelike, Cavernous, Open',
-    algo: 'Cellular',
-    algoArgs: [genWidth, genHeight, { connected: true, born: [4, 5, 6, 7, 8], survive: [2, 3, 4, 5] }],
-    randomize: 0.8,
-    iterations: 1,
-    connect: true
-  },
-  {
-    name: 'Cavelike, Cavernous, Wide Open',
-    algo: 'Cellular',
-    algoArgs: [genWidth, genHeight, { connected: true, born: [2, 4, 6, 8], survive: [2, 4, 6] }],
-    randomize: 0.9,
-    iterations: 3,
-    connect: true
-  },
-  {
-    name: 'Cavelike, Cavernous, Super Wide Open',
-    algo: 'Cellular',
-    algoArgs: [genWidth, genHeight, { connected: true, born: [4, 5, 6, 7, 8], survive: [1, 2, 3, 4, 5] }],
-    randomize: 0.9,
-    iterations: 50,
-    connect: true
-  }
-];
+const configs = config.configs.mapGen;
 
-const fluidConfigs = [
-  {
-    name: '(Wet) Uniform Maze',
-    algo: 'Uniform',
-    algoArgs: [genWidth + (gutter * 2), genHeight + (gutter * 2), { roomWidth: [3, 4], roomHeight: [3, 4], corridorLength: [3, 8], roomDugPercentage: 0.3 }]
-  },
-  {
-    name: '(Wet) Cavelike, Cavernous, Open',
-    algo: 'Cellular',
-    algoArgs: [genWidth + (gutter * 2), genHeight + (gutter * 2), { born: [4, 5, 6, 7, 8], survive: [3, 4, 5] }],
-    randomize: 0.3,
-    invert: true
-  },
-];
+// each possible fluid config
+const fluidConfigs = config.configs.fluidGen;
 
 // fill a map array with wall tiles (to start)
 const generateFullMap = () => {
